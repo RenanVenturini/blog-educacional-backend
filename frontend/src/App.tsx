681de1a1906fc2598bb84a47a0add28251e86590
gabzaok <gaabmoralles@gmail.com>
2026-08-22 19:35:46 -0300

    feat: define as rotas protegidas e os estilos base

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,53 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import RotaPrivada from "./components/RotaPrivada/RotaPrivada";
+
+import Landing from "./pages/Landing/Landing";
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
 import Post from "./pages/Post/Post";
 import CreatePost from "./pages/CreatePost/CreatePost";
+import EditPost from "./pages/EditPost/EditPost";
+import Admin from "./pages/Admin/Admin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Landing />} />
+
+        <Route path="/aluno" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
 
         <Route path="/posts/:id" element={<Post />} />
 
-        <Route path="/posts/novo" element={<CreatePost />} />
+        <Route
+          path="/admin"
+          element={
+            <RotaPrivada>
+              <Admin />
+            </RotaPrivada>
+          }
+        />
+
+        <Route
+          path="/posts/novo"
+          element={
+            <RotaPrivada>
+              <CreatePost />
+            </RotaPrivada>
+          }
+        />
+
+        <Route
+          path="/posts/:id/editar"
+          element={
+            <RotaPrivada>
+              <EditPost />
+            </RotaPrivada>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
